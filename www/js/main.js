8bf3d14ca08d66d8dd6628ecdcf7981c923f1030
xocarva <correo@xoancarneiro.com>
2022-04-24 11:56:25 +0200

    add styles

--- www/js/main.js
+++ www/js/main.js
@@ -3,6 +3,10 @@
 const form = document.getElementById( 'form' );
 const input = document.getElementById( 'task-input' );
 const list = document.getElementById('list');
+const save = document.getElementById( 'save' );
+const style = document.createElement( 'style' );
+document.head.appendChild( style );
+const styleSheet = style.sheet;
 
 // Functions
 
@@ -12,21 +16,27 @@ const loadFromLocalStorage = () => {
 
 const createList = tasks => {
     list.innerHTML = '';
+    let liId = 1;
     tasks.forEach(( text, i ) => {
         const li = document.createElement( 'li' );
         list.appendChild( li );
+
+        const remove = li.appendChild( document.createElement( 'span' ) );
+        remove.id = `remove-${liId}`;
+        remove.classList = 'fa-solid fa-delete-left remove';
+        remove.addEventListener( 'click', () => removeFromLocalStorage( i ) );
+        styleSheet.insertRule(`#remove-${liId}:hover ~ #task-${liId}{text-decoration:line-through;text-decoration-thickness:3px;}`);
+
+        const taskText = li.appendChild( document.createElement( 'span' ) );
+        taskText.id = `task-${liId}`;
+        taskText.className = 'task-text';
+        taskText.textContent = text;
 
         const ram = li.appendChild( document.createElement( 'span' ) );
         ram.className = 'ram';
         ram.textContent = '🐏';
 
-        const taskText = li.appendChild( document.createElement( 'span' ) );
-        taskText.className = 'task-text';
-        taskText.textContent = text;
-
-        const remove = li.appendChild( document.createElement( 'span' ) );
-        remove.classList = 'fa-solid fa-delete-left remove';
-        remove.addEventListener( 'click', () => removeFromLocalStorage( i ) );
+        liId++;
     });
 };
 
@@ -54,5 +64,5 @@ const handleTask = e => {
 // Listeners
 
 form.addEventListener( 'submit', handleTask );
+save.addEventListener( 'click', handleTask );
 document.addEventListener( 'DOMContentLoaded', () => createList( loadFromLocalStorage() ) );
-
